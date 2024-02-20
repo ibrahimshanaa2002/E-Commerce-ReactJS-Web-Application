988cc56110dd5883bbfba7ad0e1916f97ae798b1
Ibrahim Shanaa <ibrahim.shanaa122@gmail.com>
2024-02-20 14:41:19 +0200

    last

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
-import Login from "./components/Login";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
+
+library.add(fas)
 function App() {
   return (
     <>
-      <Router>
-        <Route path="/" exact component={Login} />
-      </Router>
       <Navbar></Navbar>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+  const [isHidden, setIsHidden] = useState(false);
+
+  const handleIconClick = () => {
+    setIsHidden(true);
+  };
+
   return (
-    <>
-      <div className="popup text-center bg-black text-white p-3	">
-        <h1>
-          Sign up and get 20% off to your first order. Sign Up Now
+    <div className="relative">
+      <div
+        className={`navbar-ad bg-black text-white p-3 flex justify-between items-center transform transition-transform duration-300 ease-in-out ${
+          isHidden ? "-translate-y-full" : "translate-y-0"
+        }`}
+      >
+        <h1 className="text-center flex-grow">
+          Sign up and get 20% off your first order. Sign Up Now
         </h1>
-      </div> 
-    </>
+        <FontAwesomeIcon
+          icon={faTimes}
+          size="sm"
+          style={{ color: "#ffffff" }}
+          onClick={handleIconClick}
+        />
+      </div>
+      <div
+        className={`navbar bg-black text-white p-3 transform transition-transform duration-300 ease-in-out ${
+          isHidden ? "translate-y-0" : "translate-y-full"
+        }`}
+        style={{ marginTop: isHidden ? "-56px" : "0" }}
+      >
+        Main Navbar
+      </div>
+    </div>
   );
 };
 
